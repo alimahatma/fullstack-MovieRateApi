5f4201ae66906aaf17665562510a580d94bea5d7
alimahatma <laluallistilo.com>
2024-06-20 21:30:21 +0800

    add to model Movie and Rating

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path 
+from rest_framework import routers #todo, menyediakan kelas-kelas untuk menagani routing URL dalam aplikasi ROUTING
 
+router = routers.DefaultRouter() #todo, otomatis membuat URL, Navigasi API, List and Detail Views
 
 urlpatterns = [
-    path('new/', admin.site.urls),
+    path('', include(router.urls)),
 ]
