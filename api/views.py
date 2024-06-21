8be5f1225fee52bb6dc477b21922054f9ed3151f
alimahatma <laluallistilo.com>
2024-06-21 16:20:15 +0800

    costom METHOD, additional method at class viewset in view.py

--- api/views.py
+++ api/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Movie, Rating
 from .serializer import MovieSerialzer, RatingSerializer
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerialzer
+
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        if 'stars' in request.data:
+            movie = Movie.objects.get(id=pk)
+            print('movie title:', movie.title) 
+
+            response = {'message':'its working'}
+            return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = {'message':'You need to provide the numbers of stars'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
