f09e61982624f40c16332179429f15be8218ec19
alimahatma <laluallistilo.com>
2024-06-23 20:59:14 +0800

    setup Restriction for permission in API

--- api/serializer.py
+++ api/serializer.py
@@ -1,18 +1,24 @@
 from rest_framework import serializers
 from .models import Movie, Rating
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
+#!class ini berfungsi untuk menambahkan akses pada USER 
+#!user yang dapat ditambahkan untuk mendapatkan akses ke API
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'password')
-        extra_kwargs = {'password':{'write_only': True, 'required':True}}
+        extra_kwargs = {'password': {'write_only': True, 'required':True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+
+        #!kode ini berfungsi untuk membuat token baru setelah user dibuat
+        Token.objects.create(user=user)
         return user
 
-class MovieSerialzer(serializers.ModelSerializer):
+class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = (
--- api/views.py
+++ api/views.py
@@ -3,21 +3,24 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication #!fungsi autentikasi token 
+from rest_framework.permissions import IsAuthenticated
 from .models import Movie, Rating
-from .serializer import MovieSerialzer, RatingSerializer, UserSerializer
+from .serializer import MovieSerializer, RatingSerializer, UserSerializer
 from django.contrib.auth.models import User
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
-    serializer_class = MovieSerialzer
+    serializer_class = MovieSerializer
     #!autentikasi class ini bertujan untuk mendapatkan hak akses berupa akses token
     #!dengan kode contoh token "faba811fe68bfab32066fcd8e48271c829642764" : user dapat mengakses API ini
     authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated,)
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
@@ -27,7 +30,6 @@ class MovieViewSet(viewsets.ModelViewSet):
             stars = request.data['stars']
             user = request.user
             # print('user:', user)
-            
 
             #!kode ini bertujuan untuk meng-update rating
             #!jika rating sudah ada, maka update, jika belum ada, objek di create
@@ -47,9 +49,24 @@ class MovieViewSet(viewsets.ModelViewSet):
         else:
             response = {'message':'You need to provide the numbers of stars'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        
+    
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
     authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated,)
+
+
+    def create(self, reqiest, *args, **kwargs):
+        response = {'message':'You cant create rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, reqiest, *args, **kwargs):
+        response = {'message':'You cant update rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
 
